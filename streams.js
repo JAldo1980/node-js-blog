cf468b16f59a027a1c4765836b2c22489e999584
JAldo1980 <jamesalderman1980@gmail.com>
2023-11-01 14:08:27 +0000

    adding streams

---
+++ streams.js
@@ -0,0 +1,18 @@
+// READ STREAM
+
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./docs/blog3.txt", {
+  encoding: "utf8",
+});
+
+const writeStream = fs.createWriteStream("./docs/blog2.txt");
+
+readStream.on("data", (chunk) => {
+  console.log("---NEW CHUNK---");
+  console.log(chunk);
+  writeStream.write("\nNEW CHUNK\n");
+  writeStream.write(chunk);
+});
+
+// WRITE STREAM
